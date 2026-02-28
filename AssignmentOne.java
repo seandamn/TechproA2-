54c0aaacfab3a4fff5f058bce92961bd97ca5ddc
seandamn <seandeipadayao@gmail.com>
2026-02-28 18:36:49 +0800

    Create AssignmentOne.java with a Swing window

---
+++ AssignmentOne.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Assignment1partOne {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("My First Swing Window");
+
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
